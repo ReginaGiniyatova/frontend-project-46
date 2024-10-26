a7d87a603e1a0141ac118e05a02a555348274b5b
Regina Giniyatova <giniyatovar@yandex.ru>
2024-10-26 14:38:49 +0300

    добавлены исправления в шаг 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import gendiff from '../index.js';
-import { readFileSync } from 'node:fs';
-import { cwd } from 'node:process';
-import path from 'node:path';
+
 
 commander
     .version('0.0.1')
@@ -12,13 +10,7 @@ commander
     .description('Compares two configuration files and shows a difference.')
     .action((filepath1, filepath2) => {
         const difference = gendiff(filepath1, filepath2)
-        filepath1 = path.resolve(cwd(), filepath1);
-        filepath2 = path.resolve(cwd(), filepath2);
-
-        const json1 = readFileSync(filepath1);
-        const json2 = readFileSync(filepath2);
-
-        console.log(`${json1}, ${json2}`);
+        console.log(difference)
     })
 
 commander.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import path from 'node:path';
+
 const gendiff = (filepath1, filepath2) => {
-    
+    filepath1 = path.resolve(cwd(), filepath1);
+    filepath2 = path.resolve(cwd(), filepath2);
+
+    const json1 = readFileSync(filepath1, 'utf-8');
+    const json2 = readFileSync(filepath2, 'utf-8');
+
+    const obj1 = JSON.parse(json1);
+    const obj2 = JSON.parse(json2);
+
+    console.log(obj1, obj2);
 };
 
 export default gendiff;
